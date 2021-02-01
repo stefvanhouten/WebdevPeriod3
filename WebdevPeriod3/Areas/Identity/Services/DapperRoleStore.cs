0e506c9a5ad9ff888dd127e9c5ec74ad2dac393f
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-02-01 16:23:34 +0100

    Scaffold a role store implementation

---
+++ WebdevPeriod3/Areas/Identity/Services/DapperRoleStore.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebdevPeriod3.Areas.Identity.Entities;
+
+namespace WebdevPeriod3.Areas.Identity.Services
+{
+    // TODO: Decide whether to implement IQueryableRoleStore<Role>
+    // TODO: Decide whether to implement IRoleClaimStore<Role>
+    /// <summary>
+    /// A Dapper-based role store implementation
+    /// </summary>
+    public class DapperRoleStore : IRoleStore<Role>
+    {
+        public Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
+        {
+            // TODO: Fetch the role's normalized name from the database.
+            return role.NormalizedName ?? throw new NotImplementedException();
+        }
+
+        public async Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
+        {
+            // TODO: Fetch the role's ID from the database.
+            return role.Id ?? throw new NotImplementedException();
+        }
+
+        public async Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
+        {
+            // TODO: Fetch the role's name from the database.
+            return role.Name ?? throw new NotImplementedException();
+        }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+
+        public Task SetNormalizedRoleNameAsync(Role role, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
