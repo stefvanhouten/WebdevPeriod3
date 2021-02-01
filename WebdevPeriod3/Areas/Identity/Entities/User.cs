236983d46f7eb6aba14288a734226c69467af254
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-02-01 16:10:29 +0100

    Add a summary to User

--- WebdevPeriod3/Areas/Identity/Entities/User.cs
+++ WebdevPeriod3/Areas/Identity/Entities/User.cs
@@ -7,5 +7,8 @@ using System.Threading.Tasks;
 
 namespace WebdevPeriod3.Areas.Identity.Entities
 {
+    /// <summary>
+    /// Represents a user within our application
+    /// </summary>
     public class User : IdentityUser<string> { }
 }
