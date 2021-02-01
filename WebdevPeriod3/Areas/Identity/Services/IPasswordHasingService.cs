e110b79793efc8ea0ef5a503998e9d952ed0246b
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-02-01 12:10:57 +0100

    Define a standard interface for password hashing implementations

---
+++ WebdevPeriod3/Areas/Identity/Services/IPasswordHasingService.cs
@@ -0,0 +1,20 @@
+﻿namespace WebdevPeriod3.Areas.Identity.Services
+{
+    public interface IPasswordHasingService
+    {
+        /// <summary>
+        /// Hashes a password
+        /// </summary>
+        /// <param name="password">The password to be hashed</param>
+        /// <returns>The password's hash</returns>
+        string Hash(string password);
+
+        /// <summary>
+        /// Compares a password to a hash
+        /// </summary>
+        /// <param name="password">The password to be compared to the hash</param>
+        /// <param name="hash">The hash to be compared to the password</param>
+        /// <returns><see langword="true"/> if the password and the hash match, otherwhise <see langword="false"/></returns>
+        string Verify(string password, string hash);
+    }
+}
