bfca83e48832d50e4f71449b1b1b3ed1c9788f47
Stef van Houten <stefvanhouten@gmail.com>
2021-02-03 22:23:13 +0100

    Update AdminController to generate Users and FlaggedComments

--- WebdevPeriod3/Controllers/AdminController.cs
+++ WebdevPeriod3/Controllers/AdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebdevPeriod3.Models;
+using WebdevPeriod3.ViewModels;
 
 namespace WebdevPeriod3.Controllers
 {
@@ -15,7 +17,32 @@ namespace WebdevPeriod3.Controllers
 
         public IActionResult Users()
         {
-            return View();
+            List<User> users = new List<User>();
+            for(int i = 0; i < 30; i++)
+            {
+                users.Add(new User() { Email = $"JohnDoe{i}@gmail.com", Role = "Admin" });
+            }
+
+            AdminViewModel viewModel = new AdminViewModel()
+            {
+                Users = users
+            };
+            return View(viewModel);
+        }
+
+        public IActionResult FlaggedComments()
+        {
+            List<Comment> comments = new List<Comment>();
+            for (int i = 0; i < 30; i++)
+            {
+                comments.Add(new Comment() { Post= "Robot0", PostedBy = $"JohnDoe{i}@gmail.com", Summary = "Some racist comment" });
+            }
+
+            AdminViewModel viewModel = new AdminViewModel()
+            {
+                Comments = comments
+            };
+            return View(viewModel);
         }
     }
 }
