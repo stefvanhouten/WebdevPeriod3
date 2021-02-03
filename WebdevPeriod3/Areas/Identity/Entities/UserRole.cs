a606f1c1cb03a27d380acbe32c3f63d5cf5c2ac5
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-02-03 17:07:35 +0100

    Create UserRole

---
+++ WebdevPeriod3/Areas/Identity/Entities/UserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebdevPeriod3.Areas.Identity.Entities
+{
+    /// <summary>
+    /// Represents a link between a user and a role in our application
+    /// </summary>
+    public class UserRole : IdentityUserRole<string> { }
+}
