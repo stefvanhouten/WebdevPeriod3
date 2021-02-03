88eeb6a2a45e362137fd1fc01e7deb818ff74fc9
Stef van Houten <stefvanhouten@gmail.com>
2021-02-03 21:49:01 +0100

    Added AdminController

---
+++ WebdevPeriod3/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebdevPeriod3.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
