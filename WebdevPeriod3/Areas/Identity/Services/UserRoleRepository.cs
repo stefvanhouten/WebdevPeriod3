2de5e23f9e13629159de3c81cc65287ee098b097
Thomas Egbert Duursma <thomasduursma@outlook.com>
2021-02-04 12:18:11 +0100

    Improve documentation

--- WebdevPeriod3/Areas/Identity/Services/UserRoleRepository.cs
+++ WebdevPeriod3/Areas/Identity/Services/UserRoleRepository.cs
@@ -12,11 +12,28 @@ namespace WebdevPeriod3.Areas.Identity.Services
 {
     public class UserRoleRepository : BaseRepository
     {
+        /// <summary>
+        /// Selects the role ID from a user role
+        /// </summary>
         private readonly Expression<Func<UserRole, string>> ROLE_ID_SELECTOR = userRole => userRole.RoleId;
+        /// <summary>
+        /// Selects the user ID from a user role
+        /// </summary>
         private readonly Expression<Func<UserRole, string>> USER_ID_SELECTOR = userRole => userRole.UserId;
+
+        /// <summary>
+        /// Selects the role ID from a role
+        /// </summary>
         private readonly Expression<Func<Role, string>> RIGHT_ROLE_ID_SELECTOR = role => role.Id;
-        private readonly Expression<Func<User, string>> RIGHT_USER_ID_SELECTOR = user => user.Id;
+        /// <summary>
+        /// Selects the role name from a role
+        /// </summary>
         private readonly Expression<Func<Role, string>> RIGHT_ROLE_NAME_SELECTOR = role => role.Name;
+
+        /// <summary>
+        /// Selects the user ID from a user
+        /// </summary>
+        private readonly Expression<Func<User, string>> RIGHT_USER_ID_SELECTOR = user => user.Id;
 
         private readonly RoleRepository _roleRepository;
 
@@ -27,19 +44,23 @@ namespace WebdevPeriod3.Areas.Identity.Services
 
         public Task<IEnumerable<T>> GetRolesFieldByUserId<T>(string userId, Expression<Func<Role, T>> expression) =>
             WithConnection(connection => connection.QueryAsync<T>(
-                // Select the expression, specifying a differing left-hand table
+                // Select the expression, specifying the UserRoles table as the left-hand table
                 $"{expression.ToSelectClause(typeof(UserRole).ToTableName())} " +
-                // Join by role ID
+                // Join user roles to roles by role ID...
                 $"INNER {ROLE_ID_SELECTOR.ToJoinClause(RIGHT_ROLE_ID_SELECTOR)} " +
-                // Filter by user ID
+                // ...whereever the user ID matches the provided user ID
                 $"AND {USER_ID_SELECTOR.ToKeyValuePair(nameof(userId))};",
                 new { userId }));
 
         public Task<IEnumerable<User>> GetUsersByRoleName(string roleName) =>
             WithConnection(connection => connection.QueryAsync<User>(
+                // Select all fields from the Users table, specifying the Roles table as the left-hand table
                 $"SELECT {typeof(User).ToTableName()}.* FROM {typeof(Role).ToTableName()} " +
+                // Join roles to user roles by role ID...
                 $"INNER {RIGHT_ROLE_ID_SELECTOR.ToJoinClause(ROLE_ID_SELECTOR)} " +
+                // ...whereever the role name matches the provided role name
                 $"AND {RIGHT_ROLE_NAME_SELECTOR.ToKeyValuePair(nameof(roleName))} " +
+                // Join user roles to users by user ID
                 $"INNER {USER_ID_SELECTOR.ToJoinClause(RIGHT_USER_ID_SELECTOR)};",
                 new { roleName }));
 
@@ -47,16 +68,22 @@ namespace WebdevPeriod3.Areas.Identity.Services
             WithConnection(connection => connection.ExecuteScalarAsync<bool>(
                 $"SELECT EXISTS (" +
                 $"SELECT 1 FROM {typeof(User).ToTableName()} " +
+                // Join users to user roles by user ID
                 $"INNER {RIGHT_USER_ID_SELECTOR.ToJoinClause(USER_ID_SELECTOR)} " +
+                // Join user roles to roles by role ID...
                 $"INNER {ROLE_ID_SELECTOR.ToJoinClause(RIGHT_ROLE_ID_SELECTOR)} " +
+                // ...whereever the role name matches the provided role name
                 $"AND {RIGHT_ROLE_NAME_SELECTOR.ToKeyValuePair(nameof(roleName))} " +
                 $");",
                 new { userId, roleName }));
 
         public Task RemoveUserFromRole(string userId, string roleName) =>
             WithConnection(connection => connection.ExecuteAsync(
+                // Delete a user role
                 $"DELETE FROM {typeof(UserRole).ToTableName()} " +
+                // Join roles to user roles by role ID...
                 $"INNER {ROLE_ID_SELECTOR.ToJoinClause(RIGHT_ROLE_ID_SELECTOR)} " +
+                // ...whereever the role name matches the provided role name
                 $"AND {RIGHT_ROLE_NAME_SELECTOR.ToKeyValuePair(nameof(roleName))};"));
 
         public async Task AddUserToRole(string userId, string roleName)
