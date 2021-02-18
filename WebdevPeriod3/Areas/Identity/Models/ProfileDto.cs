e13cad97f0c43482737bca6f7154ce56fc8ae8a4
Stef van Houten <stefvanhouten@gmail.com>
2021-02-18 19:21:44 +0100

    Create ProfileDto.cs

---
+++ WebdevPeriod3/Areas/Identity/Models/ProfileDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebdevPeriod3.Areas.Identity.Entities;
+using WebdevPeriod3.Entities;
+
+namespace WebdevPeriod3.Areas.Identity.Models
+{
+    public class ProfileDto
+    {
+        public User UserInformation { get; set; }
+        public IEnumerable<Product> OwnProducts { get; set; }
+    }
+}
+
