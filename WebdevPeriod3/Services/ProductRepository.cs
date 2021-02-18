9de33924262af64cec44c4c1a76668976cc66e5a
glinxtech <gooplsarrero@gmail.com>
2021-02-18 09:56:01 -0400

    Method for fetching all posts

--- WebdevPeriod3/Services/ProductRepository.cs
+++ WebdevPeriod3/Services/ProductRepository.cs
@@ -9,34 +9,37 @@ using WebdevPeriod3.Utilities;
 
 namespace WebdevPeriod3.Services
 {
-    public class ProductRepository : TransactionRepositoryBase
-    {
-        private readonly DapperTransactionService _dapperTransactionService;
+	public class ProductRepository : TransactionRepositoryBase
+	{
+		private readonly DapperTransactionService _dapperTransactionService;
 
-        private static readonly Expression<Func<Product, string>> POSTER_ID_SELECTOR = product => product.PosterId;
-        private static readonly Expression<Func<Product, string>> ID_SELECTOR = product => product.Id;
-        public ProductRepository(DapperTransactionService dapperTransactionService, IConfiguration configuration) : base(dapperTransactionService, configuration)
-        {
-        }
+		private static readonly Expression<Func<Product, string>> POSTER_ID_SELECTOR = product => product.PosterId;
+		private static readonly Expression<Func<Product, string>> ID_SELECTOR = product => product.Id;
+		public ProductRepository(DapperTransactionService dapperTransactionService, IConfiguration configuration) : base(dapperTransactionService, configuration)
+		{
+		}
 
-        public void Add(Product product)
-        {
-            if (product.Id == null)
-                product.Id = Guid.NewGuid().ToString("N");
+		public void Add(Product product)
+		{
+			if (product.Id == null)
+				product.Id = Guid.NewGuid().ToString("N");
 
-            AddOperation(
-               (connection, transaction) => connection.ExecuteAsync(product.ToInsertQuery(), product, transaction));
-        }
-        public void Delete(Product product)
-        {
-            AddOperation(
-                (connection, transaction) => connection.ExecuteAsync(product.ToDeleteQuery(ID_SELECTOR), product, transaction));
-        }
+			AddOperation(
+			   (connection, transaction) => connection.ExecuteAsync(product.ToInsertQuery(), product, transaction));
+		}
+		public void Delete(Product product)
+		{
+			AddOperation(
+				(connection, transaction) => connection.ExecuteAsync(product.ToDeleteQuery(ID_SELECTOR), product, transaction));
+		}
 
-        public Task<IEnumerable<Product>> FindProductsByPosterId(string posterId) =>
-            WithConnection(
-               connection => connection.QueryAsync<Product>(
-                   SqlHelper.CreateSelectWhereQuery(POSTER_ID_SELECTOR, nameof(posterId)),
-                   new { posterId }));
-    }
+		public Task<IEnumerable<Product>> GetAllProducts() =>
+			WithConnection(connection => connection.QueryAsync<Product>(SqlHelper.CreateSelectQuery("products")));
+
+		public Task<IEnumerable<Product>> FindProductsByPosterId(string posterId) =>
+			WithConnection(
+			   connection => connection.QueryAsync<Product>(
+				   SqlHelper.CreateSelectWhereQuery(POSTER_ID_SELECTOR, nameof(posterId)),
+				   new { posterId }));
+	}
 }
