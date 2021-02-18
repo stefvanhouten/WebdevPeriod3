2a18bdab36b75aa7edd051cb39dc716b0939408a
Stef van Houten <stefvanhouten@gmail.com>
2021-02-18 19:21:24 +0100

    Move product

---
+++ WebdevPeriod3/Areas/Identity/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebdevPeriod3.Areas.Identity.Entities
+{
+    /// <summary>
+    /// Represents a product in our application
+    /// </summary>
+public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PosterId { get; set; }
+        public bool ShowInCatalog { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
