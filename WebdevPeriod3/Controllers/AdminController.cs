54c8ff05ae397325f75b6219a74fc51643604f70
Maurits.P <maurits.peereboom@student.nhlstenden.com>
2021-03-30 15:52:02 +0200

    create the role management ui

--- WebdevPeriod3/Controllers/AdminController.cs
+++ WebdevPeriod3/Controllers/AdminController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebdevPeriod3.Areas.Identity.Entities;
+using WebdevPeriod3.Areas.Identity.Services;
 using WebdevPeriod3.Models;
 using WebdevPeriod3.ViewModels;
 
@@ -13,18 +15,20 @@ namespace WebdevPeriod3.Controllers
     [Authorize]
     public class AdminController : Controller
     {
-        public IActionResult Users()
-        {
-            List<User> users = new List<User>();
-            for(int i = 0; i < 30; i++)
-            {
-                users.Add(new User() { Email = $"JohnDoe{i}@gmail.com" });
-            }
+        private readonly UserRepository _userRepository;
 
+        public AdminController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Users()
+        {
             AdminViewModel viewModel = new AdminViewModel()
             {
-                Users = users
+                Users = (await _userRepository.GetAll()).ToList()
             };
+
             return View(viewModel);
         }
 
@@ -33,7 +37,7 @@ namespace WebdevPeriod3.Controllers
             List<Comment> comments = new List<Comment>();
             for (int i = 0; i < 30; i++)
             {
-                comments.Add(new Comment() { Post= "Robot0", PostedBy = $"JohnDoe{i}@gmail.com", Summary = "Some racist comment" });
+                comments.Add(new Comment() { Post = "Robot0", PostedBy = $"JohnDoe{i}@gmail.com", Summary = "Some racist comment" });
             }
 
             AdminViewModel viewModel = new AdminViewModel()
